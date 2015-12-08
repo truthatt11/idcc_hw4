2548d298c703b4e368b41fb1f7d5f5dfe7da0e54
Your Name <you@example.com>
2015-12-08 06:57:02 +0000

    ok

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from django.utils import timezone
 import time, threading
 import json
 import datetime
@@ -15,40 +14,41 @@ def test():
     urllib.urlretrieve('http://data.taipei/youbike', 'youbike.gz');
     with gzip.open('youbike.gz', 'r') as f:
         output = f.read()
-        parsed = json.loads(output, object_pairs_hook=OrderedDict)
+        parsed = json.loads(output, object_pairs_hook=OrderedDict);
 #        print parsed['retVal']['0001']['mday'], "\n"
 #        print parsed['retVal']['0001']['mday'].encode('utf-8'), "\n"
         for i, val in parsed['retVal'].iteritems():
-            print val
-            sno = val['sno'].encode('utf-8')
-            sna = val['sna'].encode('utf-8')
-            sarea = val['sarea'].encode('utf-8')
-            lat = val['lat'].encode('utf-8')
-            lng = val['lng'].encode('utf-8')
-            ar = val['ar'].encode('utf-8')
-            sareaen = val[u'sareaen'].encode('utf-8')
-            snaen = val[u'snaen'].encode('utf-8')
-            aren = val[u'aren'].encode('utf-8')
-            tot = val[u'tot'].encode('utf-8')
-            sbi = val[u'sbi'].encode('utf-8')
-            mday = val[u'mday'].encode('utf-8')
-            bemp = val[u'bemp'].encode('utf-8')
-            act = val[u'act'].encode('utf-8')
-            print mday
-            print type(mday)
+#            print val
+            zsno = val['sno'].encode('utf-8')
+            zsna = val['sna'].encode('utf-8')
+            zsarea = val['sarea'].encode('utf-8')
+            zlat = val['lat'].encode('utf-8')
+            zlng = val['lng'].encode('utf-8')
+            zar = val['ar'].encode('utf-8')
+            zsareaen = val[u'sareaen'].encode('utf-8')
+            zsnaen = val[u'snaen'].encode('utf-8')
+            zaren = val[u'aren'].encode('utf-8')
+            ztot = val[u'tot'].encode('utf-8')
+            zsbi = val[u'sbi'].encode('utf-8')
+            zmday = val[u'mday'].encode('utf-8')
+            zbemp = val[u'bemp'].encode('utf-8')
+            zact = val[u'act'].encode('utf-8')
+#            print zmday
+#            print type(zmday)
             
-            mday_parsed = datetime.datetime.strptime(mday, "%Y%m%d%H%M%S")
+            mday_parsed = datetime.datetime.strptime(zmday, "%Y%m%d%H%M%S")
 
-            c = station.objects.filter(sno__startswith=sno)
-            if c is None:
-                a = station(0, sno, sna, sarea, lat, lng, ar, sareaen, snaen, aren)
+            c = station.objects.filter(sno__startswith=zsno)
+            if not c:
+                print "c is", c
+                a = station(sno=zsno, sna=zsna, sarea=zsarea, lat=zlat, lng=zlng, ar=zar, sareaen=zsareaen, snaen=zsnaen, aren=zaren)
                 a.save()
 
-            b = info(0, sno, tot, sbi, mday_parsed, bemp, act)
+            b = info(sno=zsno, tot=ztot, sbi=zsbi, mday=mday_parsed, bemp=zbemp, act=zact)
             b.save()
-            d = info.objects.filter(mday__gt=datetime.date.today())
+            d = info.objects.filter(mday__lt=datetime.date.today())
             d.delete()
 #        print type(parsed)
-        threading.Timer(5, test).start()
+    threading.Timer(5, test).start()
 
 test()
